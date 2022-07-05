3f4a084284e2feee36ec3cceda85964fc8ea14f4
Giannis Keramidas <gkeramidas@leolabs.space>
2022-07-05 14:46:49 -0700

    authorization keys

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
 import os
 
 api_url = os.environ.get('LEO_API_URL')
-leo_labs_user = os.environ.get('Access_Key')
-leo_labs_key = os.environ.get('Secret_Key')
+leo_labs_user = os.environ.get('LL_ACCESS_KEY')
+leo_labs_key = os.environ.get('LL_SECRET_KEY')
 headers = {'Authorization': ''.join(['basic ', leo_labs_user, ':', leo_labs_key])}
