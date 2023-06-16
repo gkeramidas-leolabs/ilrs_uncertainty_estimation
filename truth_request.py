66da842a8c0ff87707c5a1ac365faaae37761897
Giannis Keramidas <gkeramidas@leolabs.space>
2023-06-16 16:00:08 -0700

    Removes setting of environmental library and moves it to .bashrc (not inside repo)

--- truth_request.py
+++ truth_request.py
@@ -5,10 +5,7 @@ leo_backend_od_path = Path("/Users/gkeramidas/Projects/od-master/leo-backend-od"
 sys.path.append(str(leo_backend_od_path))
 import os
 
-os.environ["BACKEND_API_URL"] = "https://api.leolabs.space/v1"
 
-
-import os
 import re
 import datetime
 from datetime import timedelta
