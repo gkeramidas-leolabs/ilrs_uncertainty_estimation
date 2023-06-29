e81955f92121df9826102fd48fdb47473f33f3b1
Giannis Keramidas <gkeramidas@leolabs.space>
2023-06-29 14:59:31 -0700

    Adds absolute output path in scripts

---
+++ scripts/ILRS_economy.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import sys
+from pathlib import Path
+
+modules_path = Path("/Users/gkeramidas/Projects/ilrs_uncertainty_estimation")
+sys.path.append(str(modules_path))
+
+import ilrs_error_utilities as ieu
+import truth_request as tr
+
+
+targets = ["L5011"]
+end_epoch = [2023, 6, 15]
+end_year = end_epoch[0]
+end_month = end_epoch[1]
+end_day = end_epoch[2]
+length_of_search = 2
+prov1 = "hts"
+prov2 = "sgf"
+outdir = Path("/Users/gkeramidas/Projects/ilrs_uncertainty_estimation/results/")
+
+
+def main():
+
+    for leolabs_id in targets:
+        base_year, base_month, base_day = ieu.base_date(
+            [end_year, end_month, end_day], length_of_search
+        )  # base epoch from where our comparisons begin
+
+        print("leo_id:", leolabs_id)
+
+        print(base_year, base_month, base_day)
+
+        filename = f"{leolabs_id}-{base_year}-{base_month}-{base_day}-to-{end_year}-{end_month}-{end_day}_economy.csv"
+
+        directory = tr.set_up_truth_directory_for_target(leolabs_id) + "/"
+        tr.dwld_data_for_target(leolabs_id, end_epoch, length_of_search)
+        ephemerides = ieu.truth_ephems_from_directory(directory)
+        (
+            ECI_pos,
+            ECI_vel,
+            date_list,
+        ) = ieu.compare_different_provider_ephems_over_time_economy(
+            ephemerides, base_year, base_month, base_day, length_of_search, prov1, prov2
+        )
+        df = ieu.create_dataframe_from_comparison_output_economy(
+            ECI_pos, ECI_vel, date_list
+        )
+        df.to_csv(str(outdir) + filename, index=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/ILRS_sps_economy.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import sys
+from pathlib import Path
+
+modules_path = Path("/Users/gkeramidas/Projects/ilrs_uncertainty_estimation")
+sys.path.append(str(modules_path))
+
+import ilrs_error_utilities as ieu
+import truth_request as tr
+
+targets = ["L1471"]
+# targets = ["L1471", "L5429", "L3972", "L3969", "L2669"]  # single-providers
+# targets = ['L2486','L3059','L4884','L5011','L2682'] # multi-providers
+# targets = ['L5011']
+end_epoch = [2023, 6, 15]
+end_year = end_epoch[0]
+end_month = end_epoch[1]
+end_day = end_epoch[2]
+length_of_search = 3
+prov_list = ["esa", "cne"]
+preferred_prov = "esa"
+outdir = Path("/Users/gkeramidas/Projects/ilrs_uncertainty_estimation/results/")
+
+
+def main():
+    for leolabs_id in targets:
+
+        base_year, base_month, base_day = ieu.base_date(
+            [end_year, end_month, end_day], length_of_search
+        )  # base epoch from where our comparisons begin
+
+        print("leo_id:", leolabs_id)
+        print(base_year, base_month, base_day)
+        filename = f"{leolabs_id}-{base_year}-{base_month}-{base_day}-to-{end_year}-{end_month}-{end_day}_economy.csv"
+        # sets up directory and downloads relevant data files
+        directory = tr.set_up_truth_directory_for_target(leolabs_id) + "/"
+        tr.dwld_data_for_target(leolabs_id, end_epoch, length_of_search)
+
+        # Initializes tephem objects from directory files
+        ephemerides = ieu.truth_ephems_from_directory(directory)
+
+        # Main function that does the comparison
+        (
+            ECI_pos,
+            ECI_vel,
+            date_list,
+        ) = ieu.compare_single_prov_ephems_over_time_economy(
+            ephemerides,
+            base_year,
+            base_month,
+            base_day,
+            length_of_search,
+            prov_list,
+            preferred_prov,
+        )
+
+        df = ieu.create_dataframe_from_comparison_output_economy(
+            ECI_pos, ECI_vel, date_list
+        )
+        df.to_csv(str(outdir) + filename, index=False)
+
+
+if __name__ == "__main__":
+    main()
